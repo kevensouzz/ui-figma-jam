3e28116a31c17968cf7ae683d45eb3704beb0be8
Keven <kevenssouza1@gmail.com>
2023-02-19 19:30:32 -0300

    update custom edge

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { useCallback } from "react"
 import ReactFlow, { addEdge, Background, Connection, ConnectionMode, Controls, useEdgesState, useNodesState } from "reactflow"
 import 'reactflow/dist/style.css'
 import { zinc } from 'tailwindcss/colors'
+import DefaultEdge from "./components/edges/DefaultEdge"
 import Square from "./components/nodes/Square"
 
 const NODE_TYPES = {
   square: Square
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge
 }
 
 const INITIAL_NODES = [
@@ -43,12 +48,16 @@ function App() {
     <div className="w-screen h-screen">
       <ReactFlow
         nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
         nodes={nodes}
         edges={edges}
         onEdgesChange={onEdgesChanges}
         onConnect={onConnect}
         onNodesChange={onNodesChange}
         connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
         >
         <Background
         gap={12}
--- src/components/edges/DefaultEdge.tsx
+++ src/components/edges/DefaultEdge.tsx
@@ -1,11 +1,35 @@
-import { EdgeProps } from "reactflow"
+import { EdgeProps, getSmoothStepPath } from 'reactflow';
 
-const DefaultEdge = (props: EdgeProps) => {
+const DefaultEdge = ({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  data,
+  markerEnd,
+}: EdgeProps) => {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
   return (
-        <>
-        
-        </>
-    )
+      <path
+        id={id}
+        style={style}
+        className="react-flow__edge-path"
+        d={edgePath}
+        markerEnd={markerEnd}
+      />
+  );
 }
 
 export default DefaultEdge
